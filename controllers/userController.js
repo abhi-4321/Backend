5feed71b0ad23cc2dc3b4b23e20b93eb9eaf5ab8
Abhinav <abhinavmahalwal4321@gmail.com>
2023-09-22 00:40:34 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,7 @@ const signin = asynchandler(async (req, res) => {
         
         const user = await User.findOne({ email: email })
         if (!user) {
-            res.status(400).send(false)
+            res.status(404).send(false)
             return
         }
         const password = user.password
